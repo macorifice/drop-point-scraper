d9eab6a1677f92415f628e7b7e29f5a7cd47bae3
Stefano Di Cecco <s.dicecco@reply.it>
2021-05-15 12:06:40 +0200

    updated cap and logs - cap multipli missing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import axios, { AxiosResponse } from 'axios';
 import rateLimit from 'axios-rate-limit';
-import cap from './data/comuni.json';
+import cap from './data/cap.json';
 import * as fs from 'fs';
 import _ from 'lodash';
 
-// This is the structure of the Point data we recieve
 interface PointData {
   address: string;
   id: string;
@@ -16,20 +15,17 @@ interface PointData {
 }
 
 const pointCenter: PointData[] = [];
-// sets max 2 requests per 1 second, other will be delayed
-// note maxRPS is a shorthand for perMilliseconds: 1000, and it takes precedence
-// if specified both with maxRequests and perMilliseconds
+
 const http = rateLimit(axios.create(), {
   maxRequests: 2,
   perMilliseconds: 30000,
   maxRPS: 2,
 });
-http.getMaxRPS(); // 2
+http.getMaxRPS();
 
-// options hot-reloading also available
 http.setMaxRPS(3);
-http.getMaxRPS(); // 3
-http.setRateLimitOptions({ maxRequests: 6, perMilliseconds: 30000 }); // same options as constructor
+http.getMaxRPS();
+http.setRateLimitOptions({ maxRequests: 6, perMilliseconds: 30000 });
 
 Object.values(cap).map((r) => {
   http
@@ -37,27 +33,43 @@ Object.values(cap).map((r) => {
       query: r.cap[0],
     })
     .then((response: AxiosResponse) => {
-      console.log('Risultati per : ', r.cap[0]);
-      console.log(response.data.data);
+      fs.appendFile(
+        'src/result/points.log',
+        `Risultati per CAP ${r.cap[0]} : ${JSON.stringify(
+          response.data.data[0]
+        )}`,
+        (err) => {
+          if (err) throw err;
+        }
+      );
 
       if (_.isEmpty(response.data.data)) {
-        fs.appendFile('emptyCap.json', JSON.stringify(pointCenter), (err) => {
-          // In case of a error throw err.
-          if (err) throw err;
-        });
+        fs.appendFile(
+          'src/result/emptyCap.json',
+          JSON.stringify(r.cap[0]),
+          (err) => {
+            if (err) throw err;
+          }
+        );
       }
 
       pointCenter.push(_.omit(response.data.data[0], 'logo'));
 
-      fs.appendFile('pointCenter.json', JSON.stringify(pointCenter), (err) => {
-        // In case of a error append to errors file.
-        if (err) {
-          fs.appendFile('pointCenterError.json', JSON.stringify(pointCenter), (err) => {
-            // In case of a error throw err.
-            if (err) throw err;
-          });
-        };
-      });
+      fs.appendFile(
+        'src/result/pointCenter.json',
+        JSON.stringify(pointCenter),
+        (err) => {
+          if (err) throw err;
+        }
+      );
     })
-    .catch((error) => console.log(error));
+    .catch((error) => {
+      fs.appendFile(
+        'src/result/pointCenterError.json',
+        JSON.stringify(`${r.cap[0]}: ${error}`),
+        (err) => {
+          if (err) throw err;
+        }
+      );
+    });
 });
